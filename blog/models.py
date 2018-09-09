693d26b3d39f26e2a6cff95e2224fd072610a81e
Venky-GH <venkynaidu1997@gmail.com>
2018-09-09 22:27:15 +0530

    Prototype Ready - will require changes in future

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,12 @@ from django.db import models
 class Blog(models.Model):
 	title = models.CharField(max_length=100)
 	pub_date = models.DateTimeField()
-	body = models.CharField(max_length=300)
+	body = models.TextField(max_length=1000)
 	image = models.ImageField(upload_to='images/')
+
+
+	def summary(self):
+		return self.body[:70]
+
+	def __str__(self):
+		return self.title
